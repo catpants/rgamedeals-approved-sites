13a5101336f4ace9a22b10df1471c68414dc8304
catpants <catpants@gmail.com>
2022-01-04 12:16:50 -0500

    Add full list of Amazon URLs

--- get-sites-list.sh
+++ get-sites-list.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Print a list of legit web storefronts for video games based on rgamedeals.net
+# approved sites, formatted for a dyno.gg auto-delete filter
+#
+# 'pup' is from https://github.com/EricChiang/pup
 
 SITE_WHITELIST=(
     'https://isthereanydeal.com'
@@ -8,7 +12,49 @@ SITE_WHITELIST=(
     'https://psprices.com'
 )
 
-# 'pup' is from https://github.com/EricChiang/pup
+# Search 'icp-country-map' on
+# https://www.amazon.ca/gp/navigation-country/select-country/
+# for regional TLDs. gaming.amazon.com is added manually
+AMAZON_URLS=(
+    'https://gaming.amazon.com'
+    'https://www.amazon.ae'
+    'https://www.amazon.ca'
+    'https://www.amazon.cn'
+    'https://www.amazon.co.jp'
+    'https://www.amazon.co.uk'
+    'https://www.amazon.com'
+    'https://www.amazon.com.au'
+    'https://www.amazon.com.br'
+    'https://www.amazon.com.mx'
+    'https://www.amazon.com.tr'
+    'https://www.amazon.de'
+    'https://www.amazon.eg'
+    'https://www.amazon.es'
+    'https://www.amazon.fr'
+    'https://www.amazon.in'
+    'https://www.amazon.it'
+    'https://www.amazon.nl'
+    'https://www.amazon.pl'
+    'https://www.amazon.sa'
+    'https://www.amazon.se'
+    'https://www.amazon.sg'
+)
+
+# Fetch a store page from rgamedeals.net and scrape the link to the store
+# website, then print it to stdout
+# $1: An rgamedeals.net store URL, e.g. "https://rgamedeals.net/store/y1odg"
+function print_store_url() {
+    STORE_URL=$(curl -s "${1}" | pup '.jumbotron > .lead > a attr{href}')
+    [ -z "${STORE_URL}" ] && return
+
+    # If www.amazon.com is approved, print the full list of amazon.com URLs instead
+    if [ "${STORE_URL}" = "https://www.amazon.com" ]; then
+        (IFS="|"; printf "|%s" "${AMAZON_URLS[*]}")
+    else
+        printf "|%s" "${STORE_URL}"
+    fi
+}
+
 STORE_LINKS=$(curl -s "https://rgamedeals.net/" | pup '.store-approved attr{href}')
 if [ -z "${STORE_LINKS}" ]; then
     echo "Couldn't get store links"
@@ -17,19 +63,13 @@ fi
 
 (IFS="|"; printf "%s" "${SITE_WHITELIST[*]}")
 
-function print_store_url() {
-    STORE_URL=$(curl -s "https://rgamedeals.net${1}" | pup '.jumbotron > .lead > a attr{href}')
-    [ -z "${STORE_URL}" ] && return
-    printf "|%s" "${STORE_URL}"
-}
-
 for LINK in $STORE_LINKS; do
     if [[ "${LINK}" != /store/* ]]; then
-        echo "Error: invalid store link: '${LINK}'"
-        exit 1
+        echo "Skipping invalid store link: '${LINK}'"
+        continue
     fi
 
-    print_store_url $LINK &
+    print_store_url "https://rgamedeals.net${LINK}" &
 done
 
 wait
